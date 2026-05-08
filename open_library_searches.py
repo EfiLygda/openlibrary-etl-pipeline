af6608493141db087db435b22828eb72576f4b41
EfiLygda <efi1031995@gmail.com>
2026-05-08 14:48:58 +0300

    Added a wrapper for requests with error handling, and a basic search function for quering the API.

--- open_library_searches.py
+++ open_library_searches.py
@@ -12,6 +12,54 @@ import requests
 # ---------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------
+# --- Parameters for quering the API ---
+# Limiting to 100 records per page and maximum 20 pages
+LIMIT = 100
+
+# Setting the connection and reading timeouts
+CONNECT_TIMEOUT = 15
+READ_TIMEOUT = 15
+# ---------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------
+def request_wrapper(
+        base_url: str,
+        request_params: dict,
+        connect_timeout: float = CONNECT_TIMEOUT,
+        read_timeout: float = READ_TIMEOUT
+) -> dict | None :
+    """
+    Basic wrapper for requests with error handling
+
+    :param base_url : str, the base url for the request
+    :param request_params: dict, dictionary containing the parameters of the request
+    :param connect_timeout: float, connection timeout
+    :param read_timeout: float, reading timeout
+    :return: dict | None, returns JSON response or None in case there was an error
+    """
+
+    try:
+        # Querying the API
+        response = requests.get(
+            base_url,
+            params=request_params,
+            timeout=(connect_timeout, read_timeout)
+        )
+
+        # If the URL is invalid or returns a 4xx/5xx status code, it raises an HTTPError.
+        response.raise_for_status()
+
+        # Returns the JSON response as a dictionary
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        # Print error message and return None
+        print(f"Request failed: {e}")
+        return None
+# ---------------------------------------------------------------------------------------------------------
+
+
+# ---------------------------------------------------------------------------------------------------------
 # --- Search  ---
 
 # Example: https://openlibrary.org/search.json?subject={SUBJECT}&page={PAGE}&limit={LIMIT}
@@ -19,23 +67,35 @@ import requests
 #          limiting to certain number of records by page and in a specified page of the results
 
 # Source for more fields: https://openlibrary.org/dev/docs/api/search
-
 SEARCH_BASE_URL = 'https://openlibrary.org/search.json'
 
-def search_open_library(request_params, **kwargs):
-    response = requests.get(
-        SEARCH_BASE_URL,
-        params=request_params,
-        timeout=(kwargs['CONNECT_TIMEOUT'], kwargs['READ_TIMEOUT'])
-    )
+def search_OL(limit: int = LIMIT, **kwargs) -> dict | None:
+    """
+    Function for quering Open Library API to retrieve book data
+    :param limit: int, maximum number of records by page
+    :param kwargs:  dict, dictionary containing additional parameters for the query (Read more: https://openlibrary.org/dev/docs/api/search)
+    :return: dict | None, returns JSON response or None in case there was an error
+    """
 
-    return response
+    # Set up the parameters for the query
+    request_params = {'limit': limit}
+
+    # Add the new parameters as passed
+    request_params.update(kwargs)
+
+    # Request and return the JSON response
+    return request_wrapper(SEARCH_BASE_URL, request_params)
 # ---------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------
-# --- Specific Edition of Book ---
+# TODO: https://openlibrary.org/books/OL45650119M.json
+#       OL45650119M is the edition_key and it returns info
 
-# Example: https://openlibrary.org/works/{edition_key}.json
-#          This returns records in JSON format for a specific edition of a book
+# TODO: https://openlibrary.org/search/authors.json?q=romance&limit=1
+#       this searches for romance authors
 
-EDITION_BASE_URL = 'https://openlibrary.org/works/{edition_key}.json'
+# TODO: https://openlibrary.org/authors/OL7412785A.json
+#       this searches for specific author data
+
+# TODO: https://openlibrary.org/api/get_many?keys=%5B%22/authors/OL7412785A%22,%22/authors/OL7300387A%22%5D
+#       this returns many authors or books
