eca776ae7a4ea3f0c60bec1f8f1bf9311fed6bc6
EfiLygda <efi1031995@gmail.com>
2026-05-11 00:58:17 +0300

    Merged dataset_initilize.py to config.py and deleted it.

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
 import os
-from dataset_initilize import RAW_PAGES_DIR
 
-# --- Setting up the genre directories ---
+# Setting up the genre
 GENRE = 'romance fiction'
 
 # A normalized version of the genre used for filenames and directories
 GENRE_facet = GENRE.replace(' ', '_')
 
-# Creating the raw files directory, if it does not already exists
+# Setting up the directories
+ROOT_DIR = './'
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+CSV_DIR = os.path.join(DATA_DIR, 'csv')
+RAW_PAGES_DIR = os.path.join(DATA_DIR, 'raw_pages')
 GENRE_DIR = os.path.join(RAW_PAGES_DIR, GENRE_facet)
-if not os.path.exists(GENRE_DIR):
-    os.makedirs(GENRE_DIR)
+
+# List containing all new directories
+NEW_DIRS = [DATA_DIR, CSV_DIR, RAW_PAGES_DIR, GENRE_DIR]
+
+# Creating new directories, if they do not already exist
+for directory in NEW_DIRS:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
