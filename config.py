5eb58059732a159b88d3457de36df5912ea8f5bd
EfiLygda <efi1031995@gmail.com>
2026-05-11 17:52:32 +0300

    Added new dirs in config.py

--- config.py
+++ config.py
@@ -12,9 +12,18 @@ DATA_DIR = os.path.join(ROOT_DIR, 'data')
 CSV_DIR = os.path.join(DATA_DIR, 'csv')
 RAW_PAGES_DIR = os.path.join(DATA_DIR, 'raw_pages')
 GENRE_DIR = os.path.join(RAW_PAGES_DIR, GENRE_facet)
+WORKS_DIR = os.path.join(GENRE_DIR, 'works')
+KEYS_DIR = os.path.join(DATA_DIR, 'keys')
 
 # List containing all new directories
-NEW_DIRS = [DATA_DIR, CSV_DIR, RAW_PAGES_DIR, GENRE_DIR]
+NEW_DIRS = [
+    DATA_DIR,
+    CSV_DIR,
+    RAW_PAGES_DIR,
+    GENRE_DIR,
+    WORKS_DIR,
+    KEYS_DIR
+]
 
 # Creating new directories, if they do not already exist
 for directory in NEW_DIRS:
