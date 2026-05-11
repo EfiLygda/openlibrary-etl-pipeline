902f837fcdde824bc0b9168bdb6b6a062adfb171
EfiLygda <efi1031995@gmail.com>
2026-05-12 00:25:46 +0300

    Added the main frame for querying and saving in JSON files works', authors' and series' metadata for the works returned from 1_query_works_via_SEARCH.py

--- 3_query_works_authors_series_via_keys.py
+++ 3_query_works_authors_series_via_keys.py
@@ -1,13 +1,29 @@
 import os
+import re
 import json
-from config import GENRE_facet, KEYS_DIR, WORKS_DIR, AUTHORS_DIR, SERIES_DIR
+from config import GENRE, KEYS_DIR, WORKS_DIR, AUTHORS_DIR, SERIES_DIR
 from OpenLibrary import OpenLibraryClient
 
 client = OpenLibraryClient()
 
-for key_file in os.listdir(KEYS_DIR):
+key_type_pattern = r'.+_(work|author|series)_.+\.json$'
 
-    key_file_path = os.path.join(KEYS_DIR, key_file)
+dirs = {
+    'work': WORKS_DIR,
+    'author': AUTHORS_DIR,
+    'series': SERIES_DIR
+}
+
+for key_file_name in os.listdir(KEYS_DIR):
+
+    key_file_path = os.path.join(KEYS_DIR, key_file_name)
+
+    key_type_match = re.search(key_type_pattern, key_file_name)
+
+    if not key_type_match:
+        raise ValueError(f'Key type was not found in filename: {key_file_name}')
+
+    key_file_type = key_type_match.group(1)
 
     with open(key_file_path, mode='r', encoding='utf-8') as j:
         keys_list = json.load(j)
@@ -19,6 +35,21 @@ for key_file in os.listdir(KEYS_DIR):
         for i in range(0, len(keys_list), batch_size)
     ]
 
-    for key_batch in key_batches:
-        data = client.get_many(key_batch)
-        print(data)
+    for i, key_batch in enumerate(key_batches):
+        records = client.get_many(key_batch)
+
+        if not records['result']:
+            raise ValueError(f'No results were returned from {client.last_url}')
+
+        batch_filename = f'{key_file_type.upper()}_p{i+1}.json'
+        batch_filepath = os.path.join(dirs[key_file_type], batch_filename)
+        client.save_json(records, batch_filepath)
+
+        if i + 1 == len(key_batches):
+            end_str = '\n'
+        else:
+            end_str = '\r'
+
+        print(f'({i+1}/{len(key_batches)}) Extracting {GENRE} {key_file_type}\'s metadata...', end=end_str)
+
+print(f'Finished extracting {GENRE} works\', authors\' and series\' metadata.')
