0cc6ee5ff65f2d6fef47171d6c9256b89446b74f
EfiLygda <efi1031995@gmail.com>
2026-05-14 19:53:06 +0300

    Added comments and KeyHandler.get_key at keys.py

--- open_library/keys.py
+++ open_library/keys.py
@@ -1,3 +1,4 @@
+import re
 import json
 
 class KeyHandler:
@@ -14,14 +15,22 @@ class KeyHandler:
     ]
 
     # Basic Open Library IDs regex
-    WORK_ID_PATTERN = r'OL\d+W'
-    AUTHOR_ID_PATTERN = r'OL\d+A'
-    BOOK_ID_PATTERN = r'OL\d+M'
-    SERIES_ID_PATTERN = r'OL\d+L'
+    key_patterns = {
+        'work': r'OL\d+W',
+        'author': r'OL\d+A',
+        'edition': r'OL\d+M',
+        'series': r'OL\d+L'
+    }
 
     @staticmethod
     def detect_key(key: str) -> str | None:
-
+        """
+        Function for detecting the type of Open Library key.
+        :param key: str, the string that contains the key
+        :return: str, returns one between these values ["author", "work", "series", "edition",
+                      "subject_time", "subject_person", "subject", "publisher"] or None if the type
+                      cannot be detected
+        """
         if not key.startswith('/') and not key.startswith('OL'):
             key = '/' + key
 
@@ -46,7 +55,11 @@ class KeyHandler:
 
     @staticmethod
     def normalize_key(key: str) -> str:
-
+        """
+        Function for normalizing a key by adding the right prefix
+        :param key: str, the string containing the key
+        :return: str, the normalized key
+        """
         if (key.startswith("OL") or key.startswith("author:OL")) and key.endswith("A"):
             return f"/authors/{key.replace('author:', '')}"
         elif (key.startswith("OL") or key.startswith("work:OL")) and key.endswith("W"):
@@ -72,4 +85,49 @@ class KeyHandler:
 
     @staticmethod
     def key_list_2_str(lst: list[str]) -> str:
+        """
+        Function for converting a list of keys to string representation of the list
+        :param lst: lst[str], list containing the keys
+        :return: str, string representation of the list
+        """
         return json.dumps(lst)
+
+    @staticmethod
+    def get_key(text: str, mode: str = '') -> str | None:
+        """
+        Function for extracting the key from a string
+        :param text: str, the string that contains the key
+        :param mode: str, the type of key given. Value must be in ['work','author','book','series']
+        :return: str | None, the key denormalized or None if it is not found
+        """
+
+        # If the mode is not given then there will be an attempt to detect it
+        # If it is not detected then the key will not be found and None if it is not found
+        if not mode:
+            mode = KeyHandler.detect_key(text)
+        else:
+            return None
+
+        # If the mode is not in the predetermined values then a ValueError is raised
+        if mode not in KeyHandler.key_patterns.keys():
+            raise ValueError(
+                f'Value \'{mode}\' for argument \'mode\' not valid. Use \'work\', \'author\', \'book\' or \'series\'.'
+            )
+
+        # Find all matches for the key
+        # It is noted what for this function the text is expected to only return one match
+        matches = re.findall(KeyHandler.key_patterns[mode], text)
+
+        # Checking if there are matches for the key pattern
+        if matches:
+
+            # If more than one match is found then the text is invalid and a ValueError is returned
+            if len(matches) > 1:
+                raise ValueError(f'\'text\': {text} contains more than one keys')
+
+            # Else the right match - key is returned
+            return matches[0]
+
+        else:
+            # If no matches were found then None is returned
+            return None
