4531970af8ba8ecfd1d9bc26786180697c393120
EfiLygda <efi1031995@gmail.com>
2026-05-15 16:01:29 +0300

    Added comments biography extraction for authors table in 1_author_tables.py

--- create_tables/1_author_tables.py
+++ create_tables/1_author_tables.py
@@ -1,32 +1,71 @@
+"""
+STEP 1:
+
+DETAILS:
+1.
+"""
+
 import os
+from unittest.mock import inplace
+
 import numpy as np
 import pandas as pd
-from config import AUTHORS_DIR, AUTHORS_STATISTICS_DIR, GENRE_facet, KEYS_DIR, WORKS_DIR
+from config import AUTHORS_DIR, AUTHORS_STATISTICS_DIR, GENRE_facet, WORKS_DIR, CSV_DIR
 from utilities import find_year
 from open_library import JSONFileHandler, KeyHandler
 
+# ---------------------------------------------
+# --- Authors Table ---
 
+# Fetching all authors JSON filenames
 author_files = [
     os.path.join(AUTHORS_DIR, filename)
     for filename in os.listdir(AUTHORS_DIR)
 ]
 
+# Dictionary that will contain all authors' data
+# Keys: author keys (i.e. '/authors/OLxxxxA')
+# Values: an author's record as returned via an AUTHORS query
 all_author_records = dict()
 
+# For each file all authors' data are loaded and updated in the dictionary
 for filename in author_files:
+
+    # Load JSON authors' data
     data = JSONFileHandler.load_json(filename)
+
+    # Update the dictionary
     all_author_records.update(data['result'])
 
+# Convert the authors' data dictionary to a dataframe and transpose in order
+# to have the author keys as index
+# Notes:
+# 1. the current index has values as '/authors/OLxxxxA' -> the keys are going to be denormalized
+# 2. column 'key' also contains each author's keys
 df_authors = pd.DataFrame(all_author_records).T
+
+# Rename 'name' column to 'author_name'
 df_authors.rename(columns={'name': 'author_name'}, inplace=True)
 
+# Check if any of the keys are wrong
+keys_are_right = (df_authors.index == df_authors.key).all()
+
+if keys_are_right:
+    print('All authors\' records were checked and author keys are right.')
+else:
+    print('All authors\' records were checked and some author keys are wrong.')
+
+# Reset index as to add it as a column
+# Denormalize index as to extract pure authors' key
 new_index = [KeyHandler.get_key(i) for i in df_authors.index]
+
+# Set the new denormalized index as the current index
 df_authors.index = new_index
 
+# Reset index as to now be a new column, and rename the column to 'author_key'
 df_authors.reset_index(inplace=True, names='author_key')
 
-# ---------------------------------------------
-# 1. AUTHORS_TABLE
+# Column names to keep for the final table
 authors_table_columns_to_keep = [
     'author_key',
     'author_name',
@@ -35,10 +74,39 @@ authors_table_columns_to_keep = [
     'death_date',
 ]
 
+# Keep only wanted columns
 authors_table = df_authors[authors_table_columns_to_keep]
 
+# Extract birth and death year for authors
 authors_table['birth_year'] = df_authors.birth_date.apply(find_year)
 authors_table['death_year'] = df_authors.death_date.apply(find_year)
+
+# Extract authors' bio
+authors_table['bio'] = authors_table.bio.apply(
+    lambda x: x['value']
+    if isinstance(x, dict) and 'value' in x.keys()
+    else np.nan
+)
+
+# Set data types for each column
+authors_dtypes = {
+    'author_key': 'string',
+    'author_name': 'string',
+    'bio': 'string',
+    'birth_date': 'string',
+    'death_date': 'string',
+    'birth_year': 'Int64',
+    'death_year': 'Int64'
+}
+
+authors_table = authors_table.astype(authors_dtypes)
+
+# Export authors' table as a CSV file
+authors_table.to_csv(
+    os.path.join(CSV_DIR, 'authors.csv'),
+    index=False,
+
+)
 # ---------------------
 
 # ---------------------
@@ -108,5 +176,4 @@ works_authors_table['author_key'] = works_authors_table.authors.apply(
 )
 works_authors_table.drop('authors', inplace=True, axis=1)
 # ---------------------
-
 
