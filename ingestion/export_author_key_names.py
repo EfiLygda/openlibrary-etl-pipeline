4f17665e74f0bdca2c391cc705db2e68ae7fea19
EfiLygda <efi1031995@gmail.com>
2026-05-20 17:55:31 +0300

    Added logging to export_author_key_names.py and fetch_works_authors_series.py

--- ingestion/export_author_key_names.py
+++ ingestion/export_author_key_names.py
@@ -13,76 +13,87 @@ from config.paths import AUTHORS_DIR, KEYS_DIR
 from config.api import GENRE_facet
 from open_library import Client
 from utilities.io import load_json, save_json
+from utilities.logging import set_logger
 
-# Setting up thw Open Library client for querying the API
-client = Client()
+logger = set_logger('EXPORT_AUTHOR_KEY_NAMES')
 
-# Setting up the dictionary that will contain the final author key, name pairs
-author_key_names = dict()
+def run():
+    logger.info(f'Starting exporting of all authors\' keys and names')
 
-# Count how many redirected records will be
-redirected_records = 0
+    # Setting up thw Open Library client for querying the API
+    client = Client()
 
-# For each raw pages that were returned for authors all author key and names
-# are extracted
-for filename in os.listdir(AUTHORS_DIR):
+    # Setting up the dictionary that will contain the final author key, name pairs
+    author_key_names = dict()
 
-    # Find the current author page's file path
-    filepath = os.path.join(AUTHORS_DIR, filename)
+    # Count how many redirected records will be
+    redirected_records = 0
 
-    # Opening the current JSON file
-    data = load_json(filepath)
+    # For each raw pages that were returned for authors all author key and names
+    # are extracted
+    for filename in os.listdir(AUTHORS_DIR):
 
-    # Extracting the author records
-    records = data['result']
+        # Find the current author page's file path
+        filepath = os.path.join(AUTHORS_DIR, filename)
 
-    # For each record, a check is done in case it is not an author's record
-    # but a redirect instead
-    for key, record in records.items():
+        # Opening the current JSON file
+        data = load_json(filepath)
 
-        # If the record is a redirect then the author's right record is fetched
-        if client.is_redirect(record):
+        # Extracting the author records
+        records = data['result']
 
-            # Add to rediricted records counter
-            redirected_records += 1
+        # For each record, a check is done in case it is not an author's record
+        # but a redirect instead
+        for key, record in records.items():
 
-            # Fetch the record that the original redirects to
-            record = client.get_redirected_record(record)
+            # If the record is a redirect then the author's right record is fetched
+            if client.is_redirect(record):
 
-            # Export it to the right format
-            record_to_export = {'result': {record['key']: record}}
-            save_json(
-                record_to_export,
-                os.path.join(AUTHORS_DIR, f'redirected_{redirected_records}.json')
-            )
+                # Add to redirected records counter
+                redirected_records += 1
 
-            print(
-                f'Key \'{key}\' redirects to \'{record['key']}\'. Added new raw page but original stays as is.'
-            )
+                # Fetch the record that the original redirects to
+                record = client.get_redirected_record(record)
 
-        # The current record's author name is extracted
-        new_name = record['name']
-
-        # A check is done in case alternative names for the same author key
-        # are found
-        if key in author_key_names:
-
-            # Save the original name that was saved in a previous iteration
-            old_name = author_key_names[key]['name']
-
-            # In case the previous name and the current new are different
-            # a ValueError is raised
-            if old_name != new_name:
-                raise ValueError(
-                    f'Two different names \'{old_name}\' and \'{new_name}\' for author key \'{key}\'.'
+                # Export it to the right format
+                record_to_export = {'result': {record['key']: record}}
+                save_json(
+                    record_to_export,
+                    os.path.join(AUTHORS_DIR, f'redirected_{redirected_records}.json')
                 )
 
-        # Add the author's key, name pair to the dictionary
-        author_key_names[key] = new_name
+                # print(
+                #     f'Key \'{key}\' redirects to \'{record['key']}\'. Added new raw page but original stays as is.'
+                # )
+                logger.info(
+                    f'Key \'{key}\' redirects to \'{record['key']}\'. Added new raw page but original stays as is.'
+                )
 
-# Setting up the final JSON file's name and path
-result_filename = f'{GENRE_facet}_authors_key_name.json'
-result_filepath = os.path.join(KEYS_DIR, result_filename)
+            # The current record's author name is extracted
+            new_name = record['name']
 
-# Save the final JSON file with the pairs
-save_json(author_key_names, result_filepath)
+            # A check is done in case alternative names for the same author key
+            # are found
+            if key in author_key_names:
+
+                # Save the original name that was saved in a previous iteration
+                old_name = author_key_names[key]['name']
+
+                # In case the previous name and the current new are different
+                # a ValueError is raised
+                if old_name != new_name:
+                    raise ValueError(
+                        f'Two different names \'{old_name}\' and \'{new_name}\' for author key \'{key}\'.'
+                    )
+
+            # Add the author's key, name pair to the dictionary
+            author_key_names[key] = new_name
+
+    # Setting up the final JSON file's name and path
+    result_filename = f'{GENRE_facet}_authors_key_name.json'
+    result_filepath = os.path.join(KEYS_DIR, result_filename)
+
+    # Save the final JSON file with the pairs
+    save_json(author_key_names, result_filepath)
+
+    logger.info(f'Finished exporting all authors\' keys and names')
--- ingestion/fetch_works_authors_series.py
+++ ingestion/fetch_works_authors_series.py
@@ -16,82 +16,98 @@ import os
 import re
 from config.paths import KEYS_DIR, WORKS_DIR, AUTHORS_DIR, SERIES_DIR
 from config.api import GENRE
+from utilities.rate_limit import wait
 from open_library import Client
 from utilities.io import load_json, save_json
 from utilities.batching import make_batches
+from utilities.logging import set_logger
 
-# Setting up thw Open Library client for querying the API
-client = Client()
+logger = set_logger('FETCH_WORKS_AUTHORS_SERIES')
 
-# The regex used for later extracting the type of keys in the JSON
-# keys files extracted from export_keys.py
-key_type_pattern = r'.+_(work|author|series)_.+\.json$'
+def run():
 
-# Connecting the type of keys with their respective saved directories
-dirs = {
-    'work': WORKS_DIR,
-    'author': AUTHORS_DIR,
-    'series': SERIES_DIR
-}
+    logger.info(
+        f'Starting extraction of all work, author and series keys via WORKS_ENRICHED, AUTHORS and SERIES queries'
+    )
 
-# For each key file the API will be queried in batches using as batch size
-# client.LIMIT and the results will be saved in JSON files
-for key_file_name in os.listdir(KEYS_DIR):
+    # Setting up thw Open Library client for querying the API
+    client = Client()
 
-    # The file path of the current key file
-    key_file_path = os.path.join(KEYS_DIR, key_file_name)
+    # The regex used for later extracting the type of keys in the JSON
+    # keys files extracted from export_keys.py
+    key_type_pattern = r'.+_(work|author|series)_.+\.json$'
 
-    # Finding if there is the key type in the name of the file
-    key_type_match = re.search(key_type_pattern, key_file_name)
+    # Connecting the type of keys with their respective saved directories
+    dirs = {
+        'work': WORKS_DIR,
+        'author': AUTHORS_DIR,
+        'series': SERIES_DIR
+    }
 
-    # If a match is not made a ValueError is raised
-    if not key_type_match:
-        print(f'Key type was not found in filename: {key_file_name}')
-        continue
+    # For each key file the API will be queried in batches using as batch size
+    # client.LIMIT and the results will be saved in JSON files
+    for key_file_name in os.listdir(KEYS_DIR):
 
-    # Extracting the key type from the file name
-    key_file_type = key_type_match.group(1)
+        # The file path of the current key file
+        key_file_path = os.path.join(KEYS_DIR, key_file_name)
 
-    # Loading the current key list
-    keys_list = load_json(key_file_path)
+        # Finding if there is the key type in the name of the file
+        key_type_match = re.search(key_type_pattern, key_file_name)
 
-    # Setting the batch size for querying
-    batch_size = client.LIMIT
+        # If a match is not made a ValueError is raised
+        if not key_type_match:
+            logger.info(f'Key type was not found in filename: {key_file_name}')
+            continue
 
-    # Creating the batches from the key list
-    key_batches = make_batches(keys_list, batch_size)
+        # Extracting the key type from the file name
+        key_file_type = key_type_match.group(1)
 
-    # For each batch the API is queried and the results are saved
-    for i, key_batch in enumerate(key_batches):
+        # Loading the current key list
+        keys_list = load_json(key_file_path)
 
-        # Querying the API for the current batch
-        records = client.get_many(key_batch)
+        # Setting the batch size for querying
+        batch_size = client.LIMIT
 
-        # If no records are returned a ValueError is returned
-        if not records['result']:
-            raise ValueError(f'No results were returned from {client.last_url}')
+        # Creating the batches from the key list
+        key_batches = make_batches(keys_list, batch_size)
 
-        # The file name for the current batch
-        batch_filename = f'{key_file_type.upper()}_p{i+1}.json'
+        # For each batch the API is queried and the results are saved
+        for i, key_batch in enumerate(key_batches):
 
-        # Finding the current key type directory to save the records
-        key_type_dir = dirs[key_file_type]
+            # Querying the API for the current batch
+            records = client.get_many(key_batch)
 
-        # The file path for the current batch
-        batch_filepath = os.path.join(key_type_dir, batch_filename)
+            # If no records are returned a ValueError is returned
+            if not records['result']:
+                raise ValueError(f'No results were returned from {client.last_url}')
 
-        # Saving the current batch
-        save_json(records, batch_filepath)
+            # The file name for the current batch
+            batch_filename = f'{key_file_type.upper()}_p{i+1}.json'
 
-        # Setting up the progress message for each key types
-        # New line when the type changes, same row for batches in the same key type
-        if i + 1 == len(key_batches):
-            end_str = '\n'
-        else:
-            end_str = '\r'
+            # Finding the current key type directory to save the records
+            key_type_dir = dirs[key_file_type]
 
-        # Progress message
-        print(f'({i+1}/{len(key_batches)}) Extracting {GENRE} {key_file_type}\'s metadata...', end=end_str)
+            # The file path for the current batch
+            batch_filepath = os.path.join(key_type_dir, batch_filename)
 
-# Success message
-print(f'Finished extracting {GENRE} works\', authors\' and series\' metadata.')
+            # Saving the current batch
+            save_json(records, batch_filepath)
+
+            # Setting up the progress message for each key types
+            # New line when the type changes, same row for batches in the same key type
+            # if i + 1 == len(key_batches):
+            #     end_str = '\n'
+            # else:
+            #     end_str = '\r'
+
+            # Progress message
+            # print(f'({i+1}/{len(key_batches)}) Extracting {GENRE} {key_file_type}\'s metadata...', end=end_str)
+
+            logger.info(f'({i+1}/{len(key_batches)}) Extracted {GENRE} {key_file_type}\'s metadata')
+
+            wait(3)
+
+    # Success message
+    # print(f'Finished extracting {GENRE} works\', authors\' and series\' metadata.')
+
+    logger.info(f'Finished extracting {GENRE} works\', authors\' and series\' metadata.')
