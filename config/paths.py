1cd50463b409426af51d4ac443a73f0768609d0e
EfiLygda <efi1031995@gmail.com>
2026-05-30 13:26:14 +0300

    Refactored config.path.py as to now only include the path definitions for the directories of the project and added init_project.py that creates them

--- config/paths.py
+++ config/paths.py
@@ -1,5 +1,5 @@
 """
-Setting up the directories
+The directories' definitions
 """
 
 import os
@@ -8,51 +8,24 @@ from config.api import GENRE_facet
 # Project directory
 ROOT_DIR = os.path.abspath('./')
 
-# Data directory
-# DATA_ROOT_DIR = os.path.abspath('../')
-# DATA_DIR = os.path.join(DATA_ROOT_DIR, 'data')
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-
-# Raw JSON files directory root directory
-RAW_PAGES_DIR = os.path.join(DATA_DIR, 'raw_pages')
-GENRE_DIR = os.path.join(RAW_PAGES_DIR, GENRE_facet)
-
-# Raw JSON files directories
-SEARCH_DIR = os.path.join(GENRE_DIR, 'works')
-KEYS_DIR = os.path.join(DATA_DIR, 'keys')
-WORKS_DIR = os.path.join(GENRE_DIR, 'works_enriched')
-AUTHORS_DIR = os.path.join(GENRE_DIR, 'authors')
-SERIES_DIR = os.path.join(GENRE_DIR, 'series')
-BOOKS_DIR = os.path.join(GENRE_DIR, 'books')
-# PUBLISHERS_DIR = os.path.join(KEYS_DIR, 'publishers')
-# SUBJECTS_DIR = os.path.join(KEYS_DIR, 'subjects')
-AUTHORS_STATISTICS_DIR = os.path.join(GENRE_DIR, 'author_statistics')
-
-# Processed CSV files directory
-CSV_DIR = os.path.join(DATA_DIR, 'csv')
-
 # Log messages directories
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 
-# List containing all new directories
-NEW_DIRS = [
-    DATA_DIR,
-    CSV_DIR,
-    RAW_PAGES_DIR,
-    GENRE_DIR,
-    SEARCH_DIR,
-    KEYS_DIR,
-    WORKS_DIR,
-    AUTHORS_DIR,
-    SERIES_DIR,
-    BOOKS_DIR,
-    # PUBLISHERS_DIR,
-    # SUBJECTS_DIR,
-    AUTHORS_STATISTICS_DIR,
-    LOG_DIR
-]
+# Data directory
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+GENRE_DIR = os.path.join(DATA_DIR, GENRE_facet)
 
-# Creating new directories, if they do not already exist
-for directory in NEW_DIRS:
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+# Raw JSON files directory root directory
+RAW_PAGES_DIR = os.path.join(GENRE_DIR, 'raw')
+KEYS_DIR = os.path.join(GENRE_DIR, 'staging', 'keys')
+CSV_DIR = os.path.join(GENRE_DIR, 'processed')
+
+# Raw JSON files directories
+SEARCH_DIR = os.path.join(RAW_PAGES_DIR, 'works')
+WORKS_DIR = os.path.join(RAW_PAGES_DIR, 'works_enriched')
+AUTHORS_DIR = os.path.join(RAW_PAGES_DIR, 'authors')
+SERIES_DIR = os.path.join(RAW_PAGES_DIR, 'series')
+BOOKS_DIR = os.path.join(RAW_PAGES_DIR, 'books')
+# PUBLISHERS_DIR = os.path.join(KEYS_DIR, 'publishers')
+# SUBJECTS_DIR = os.path.join(KEYS_DIR, 'subjects')
+AUTHORS_STATISTICS_DIR = os.path.join(RAW_PAGES_DIR, 'author_statistics')
---
+++ init_project.py
@@ -0,0 +1,28 @@
+"""
+Setting up project's directories
+"""
+
+from config.paths import *
+
+# List containing all new directories
+NEW_DIRS = [
+    DATA_DIR,
+    CSV_DIR,
+    RAW_PAGES_DIR,
+    GENRE_DIR,
+    SEARCH_DIR,
+    KEYS_DIR,
+    WORKS_DIR,
+    AUTHORS_DIR,
+    SERIES_DIR,
+    BOOKS_DIR,
+    # PUBLISHERS_DIR,
+    # SUBJECTS_DIR,
+    AUTHORS_STATISTICS_DIR,
+    LOG_DIR
+]
+
+# Creating new directories, if they do not already exist
+for directory in NEW_DIRS:
+    if not os.path.exists(directory):
+        os.makedirs(directory)
