f7091a257c507b36543a4d944fbcaa7211c7a571
EfiLygda <efi1031995@gmail.com>
2026-06-03 18:35:15 +0300

    Changed 'name' to 'series_name' at 07_works_series.sql

--- database/schema/07_works_series.sql
+++ database/schema/07_works_series.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS works_series
         work_key VARCHAR PRIMARY KEY,
         series_key VARCHAR,
         series_position VARCHAR,
-        name VARCHAR,
+        series_name VARCHAR,
 
         CONSTRAINT fk_works_series_work_key
             FOREIGN KEY (work_key)
