284cc4a67c01e60e37fd809d11d9b18001a7e4b1
EfiLygda <efi1031995@gmail.com>
2026-06-11 12:37:35 +0300

    Renamed database to openlibrary_db
    NOTE: database name is static

--- etl/load/create_database.py
+++ etl/load/create_database.py
@@ -26,11 +26,11 @@ def run():
 
     # Check if database already exists and then create
     # Source: https://stackoverflow.com/a/44512503
-    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'romance_fiction';")
+    cursor.execute("SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'openlibrary_db';")
     exists = cursor.fetchone()
     if not exists:
         logger.warning(f'DATABASE_NOT_FOUND database={GENRE_facet}')
-        cursor.execute('CREATE DATABASE romance_fiction;') # Did not inject GENRE_facet to SQL # TODO: Fix db name to be dynamic
+        cursor.execute('CREATE DATABASE openlibrary_db;')
         logger.info(f'DATABASE_CREATE_SUCCESS database={GENRE_facet}')
     else:
         logger.info(f'DATABASE_EXISTS database={GENRE_facet}')
