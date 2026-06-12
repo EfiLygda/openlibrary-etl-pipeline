bfec2ae56c1ab7640793882d9e5bad8054393d5a
EfiLygda <efi1031995@gmail.com>
2026-06-12 16:03:19 +0300

    Added validation.py for record validation during formatting of records

--- api/service.py
+++ api/service.py
@@ -1,8 +1,13 @@
 """
 Transformation utilities for formatting database query results
 """
+from api.core.validation import (duplicate_column_names,
+                                 all_rows_have_identical_values_in_duplicate_columns)
 
-def format_records(records: list | None, fields: list[str] | None) -> list:
+def format_records(
+        records: list | None,
+        fields: list[str] | None
+) -> list:
     """
     Formats list of records into a list of dictionaries
 
@@ -14,6 +19,12 @@ def format_records(records: list | None, fields: list[str] | None) -> list:
     # In case no records or fields were passed then return empty list
     if not records or not fields:
         return []
+
+    # Validate if columns with duplicate names have the same values across all records
+    if not all_rows_have_identical_values_in_duplicate_columns(records, fields):
+        raise ValueError(
+            f'Some values in duplicate columns {duplicate_column_names(fields)} do not have identical value'
+        )
 
     # Set up results list
     results = []
@@ -38,7 +49,17 @@ def format_response(
         records: list | None = None,
         column_names: list[str] | None = None
 ) -> dict:
+    """
+    Formats the API's final response
 
+    :param query: str, the query used for the API
+    :param endpoint: str, the endpoing used for quering the API
+    :param method: str, HTTP method used (GET, POST, PUT, DELETE, PATCH, OPTIONS, and HEAD)
+    :param records: list, list of records containing values
+    :param column_names: list[str], list of column names corresponding to each value in a record
+
+    :return: dict, dictionary mapping the API's responce
+    """
     if records is None:
         records = []
 
