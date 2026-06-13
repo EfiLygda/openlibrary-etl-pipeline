84c42651d42ed15fda105e7c1a6b508aa36596e1
EfiLygda <efi1031995@gmail.com>
2026-06-13 17:29:08 +0300

    Added title, description, version and license to the API

--- api/app.py
+++ api/app.py
@@ -2,6 +2,8 @@
 Run:
 uvicorn api.app:app --reload
 """
+import os
+from dotenv import load_dotenv
 
 from fastapi import FastAPI
 
@@ -10,9 +12,31 @@ from api.routers import works
 
 # TODO: add parameters when needed
 
-# Main FastAPI application instance.
+# Load variables from the .env file to the environment
+load_dotenv()
+
+# Save the hidden info to variables
+GENRE = os.getenv("GENRE")
+API_VERSION = os.getenv("API_VERSION")
+
+# Main FastAPI application instance
+
+API_description = ('A FastAPI-based API for searching and retrieving '
+                   'structured book metadata stored in a relational '
+                   'database derived from Open Library data.')
+
+license = {
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    }
+
 app = FastAPI(
-    dependencies=[DB_DEPENDENCY]
+    dependencies=[DB_DEPENDENCY],
+    title=f'{GENRE.title()} API',
+    summary=API_description,
+    version=API_VERSION,
+    docs_url='/docs',
+    license_info=license,
 )
 
 # Include routers as defined in api.routers
