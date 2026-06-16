1a739709fae27533700eef02ae4b54fe4d35cc61
EfiLygda <efi1031995@gmail.com>
2026-06-16 14:43:11 +0300

    Added limit and offset parameters to get_with_filter_key, and now all queries must use named parameters like %(key)s instead of positional parameters like %s

--- api/repository/base.py
+++ api/repository/base.py
@@ -9,7 +9,9 @@ from utilities.database import get_column_names
 def get_with_filter_key(
         connection: psycopg2.extensions.connection,
         filter_key: str,
-        query: str
+        query: str,
+        limit: int | None = None,
+        offset: int | None = None,
 ) -> tuple:
     """
     Retrieve all records associated with a given key using a structured query
@@ -24,17 +26,24 @@ def get_with_filter_key(
         * `data_column_names` - column names corresponding to the records
     """
 
+    params = {'filter_key': filter_key}
+
+    if limit is not None:
+        params['limit'] = limit
+
+    if offset is not None:
+        params['offset'] = offset
+
     # Query the database
     with connection.cursor() as cursor:
+
         # Construct the query
         query_to_execute = SQL(query)
 
         # Execute the query
         cursor.execute(
-            query_to_execute,
-            vars={
-                'filter_key': filter_key
-            }
+            query=query_to_execute,
+            vars=params
         )
 
         # Fetch all records as returned
