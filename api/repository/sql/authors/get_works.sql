ca1cdabb56783180cc58ab449498552839269f7e
EfiLygda <efi1031995@gmail.com>
2026-06-20 14:19:12 +0300

    Removed unnecessary join from authors works and added sorting of works by first_publish_year descending

--- api/repository/sql/authors/get_works.sql
+++ api/repository/sql/authors/get_works.sql
@@ -8,12 +8,10 @@ FROM
     works AS w
     INNER JOIN authors_works AS aw
     ON w.work_key = aw.work_key
-    INNER JOIN authors AS a
-    ON aw.author_key = a.author_key
 WHERE
     aw.author_key = %(filter_key)s
 ORDER BY
-    w.work_key
+    w.first_publish_year DESC
 LIMIT
     %(limit)s
 OFFSET
