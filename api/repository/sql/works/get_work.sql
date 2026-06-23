45b84cc84dfb929b43af1fa5bb54d3c9e5122837
EfiLygda <efi1031995@gmail.com>
2026-06-23 16:09:17 +0300

    Changed get_work.sql in order for returned data to keep original key order

--- api/repository/sql/works/get_work.sql
+++ api/repository/sql/works/get_work.sql
@@ -1,9 +1,15 @@
 SELECT
-    *
+    w.*
 FROM
-    works
-WHERE
-    work_key = ANY(%(filter_key)s)
+    works AS w
+    INNER JOIN
+    unnest(                                     -- 2. unnest converts list of keys to table with one column of the keys
+        %(filter_key)s::text[]                  -- 1. ::text[] converts list of key to SQL array {'key1', 'key2', ...}
+    ) WITH ORDINALITY AS k(work_key, ord)       -- 3. ORDINALITY adds a new column with order values 1,2,...
+                                                -- 4. k(work_key, ord) -> renames table to k and columns to work_key, ord
+    ON w.work_key = k.work_key
+ORDER BY                                        -- Keep original order
+    k.ord
 LIMIT
     %(limit)s
 OFFSET
