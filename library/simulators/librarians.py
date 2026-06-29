b030439948e63f0f426772e1ea3dd2c980d7b334
EfiLygda <efi1031995@gmail.com>
2026-06-29 15:24:54 +0300

    Added logging to librarians.py and users.py

--- library/simulators/librarians.py
+++ library/simulators/librarians.py
@@ -8,56 +8,68 @@ from faker import Faker
 
 from config.paths import LIBRARY_RAW, LIBRARY_TABLES
 from utilities.io.json_io import save_json
+from utilities.logging import set_logger
 
 from library.simulators.sim_config import SEED, MAX_LIBRARIANS
-from utils.emails import generate_email
+from library.simulators.utils.emails import generate_email
 
 # ---------------------------------------------------------------------------------------
+# Seeding faker for reproducible data
 Faker.seed(SEED)
+
+# Faker object for simulation
 fake = Faker()
 # ---------------------------------------------------------------------------------------
 
-# ---------------------------------------------------------------------------------------
-# --- Librarians ---
+logger = set_logger('SIMULATE_LIBRARIANS')
 
-# Setting up list of librarians
-librarians = []
+def run():
 
-for _ in range(MAX_LIBRARIANS):
+    logger.info('STAGE_START')
 
-    # Simulate first and last name
-    first_name, last_name = fake.first_name(), fake.last_name()
+    # ---------------------------------------------------------------------------------------
+    # --- Librarians ---
 
-    # Simulate librarian record
-    librarian = {
+    # Setting up list of librarians
+    librarians = []
 
-        # Use generated first and last name
-        'first_name': first_name,
-        'last_name': last_name,
+    for _ in range(MAX_LIBRARIANS):
 
-        # Use generated first and last name for email generation
-        'email': generate_email(first_name, last_name),
+        # Simulate first and last name
+        first_name, last_name = fake.first_name(), fake.last_name()
 
-        # Generate registration timestamp from before 3 years to now
-        # using ISO-8601 format (i.e. "2026-06-29T14:32:10")
-        'timestamp': fake.date_time_between(start_date='-3y', end_date='now').isoformat(),
-    }
+        # Simulate librarian record
+        librarian = {
 
-    # Add librarian records to final list of librarians
-    librarians.append(librarian)
+            # Use generated first and last name
+            'first_name': first_name,
+            'last_name': last_name,
 
-# Order librarians by timestamp
-librarians_ordered = sorted(librarians, key=lambda u: u['timestamp'])
+            # Use generated first and last name for email generation
+            'email': generate_email(first_name, last_name),
 
-# Export raw records to json
-librarians_filename_json = 'librarians.json'
-librarians_filepath_json = os.path.join(LIBRARY_RAW, librarians_filename_json)
-save_json(librarians_ordered, librarians_filepath_json)
+            # Generate registration timestamp from before 3 years to now
+            # using ISO-8601 format (i.e. "2026-06-29T14:32:10")
+            'timestamp': fake.date_time_between(start_date='-3y', end_date='now').isoformat(),
+        }
 
-# Export records to csv
-librarians_df = pd.DataFrame(librarians_ordered)
+        # Add librarian records to final list of librarians
+        librarians.append(librarian)
 
-librarians_filename_csv = 'librarians.csv'
-librarians_filepath_csv = os.path.join(LIBRARY_TABLES, librarians_filename_csv)
-librarians_df.to_csv(librarians_filepath_csv, index=False)
-# ---------------------------------------------------------------------------------------
+    # Order librarians by timestamp
+    librarians_ordered = sorted(librarians, key=lambda u: u['timestamp'])
+
+    # Export raw records to json
+    librarians_filename_json = 'librarians.json'
+    librarians_filepath_json = os.path.join(LIBRARY_RAW, librarians_filename_json)
+    save_json(librarians_ordered, librarians_filepath_json)
+
+    # Export records to csv
+    librarians_df = pd.DataFrame(librarians_ordered)
+
+    librarians_filename_csv = 'librarians.csv'
+    librarians_filepath_csv = os.path.join(LIBRARY_TABLES, librarians_filename_csv)
+    librarians_df.to_csv(librarians_filepath_csv, index=False)
+    # ---------------------------------------------------------------------------------------
+
+    logger.info('STAGE_COMPLETE')
--- library/simulators/users.py
+++ library/simulators/users.py
@@ -9,62 +9,74 @@ from faker import Faker
 
 from config.paths import LIBRARY_RAW, LIBRARY_TABLES
 from utilities.io.json_io import save_json
+from utilities.logging import set_logger
 
 from library.simulators.sim_config import SEED, MAX_USERS
-from utils.emails import generate_email
+from library.simulators.utils.emails import generate_email
 
 # ---------------------------------------------------------------------------------------
+# Seeding faker for reproducible data
 Faker.seed(SEED)
+
+# Faker object for simulation
 fake = Faker()
 # ---------------------------------------------------------------------------------------
 
-# ---------------------------------------------------------------------------------------
-# --- Users ---
+logger = set_logger('SIMULATE_USERS')
 
-# Setting up list of users
-users = []
+def run():
 
-for _ in range(MAX_USERS):
+    logger.info('STAGE_START')
 
-    # Simulate first and last name
-    first_name, last_name = fake.first_name(), fake.last_name()
+    # ---------------------------------------------------------------------------------------
+    # --- Users ---
 
-    # Simulate user record
-    user = {
-        # Use generated first and last name
-        'first_name': first_name,
-        'last_name': last_name,
+    # Setting up list of users
+    users = []
 
-        # Use generated first and last name for email generation
-        'email': generate_email(first_name, last_name),
+    for _ in range(MAX_USERS):
 
-        # Generate passwords with max 10 characters
-        'password': fake.password(length=10),
+        # Simulate first and last name
+        first_name, last_name = fake.first_name(), fake.last_name()
 
-        # Generate registration timestamp from before 3 years to now
-        # using ISO-8601 format (i.e. "2026-06-29T14:32:10")
-        'timestamp': fake.date_time_between(start_date='-3y', end_date='now').isoformat(),
+        # Simulate user record
+        user = {
+            # Use generated first and last name
+            'first_name': first_name,
+            'last_name': last_name,
 
-        # Generate whether the user is an active user or not
-        # so that more users will be active by 3:1 proportion
-        'is_active': bool(np.random.choice([True, True, True, False]))
-    }
+            # Use generated first and last name for email generation
+            'email': generate_email(first_name, last_name),
 
-    # Add user records to final list of users
-    users.append(user)
+            # Generate passwords with max 10 characters
+            'password': fake.password(length=10),
 
-# Order users by timestamp
-users_ordered = sorted(users, key=lambda u: u['timestamp'])
+            # Generate registration timestamp from before 3 years to now
+            # using ISO-8601 format (i.e. "2026-06-29T14:32:10")
+            'timestamp': fake.date_time_between(start_date='-3y', end_date='now').isoformat(),
 
-# Export raw records to json
-users_filename_json = 'users.json'
-users_filepath_json = os.path.join(LIBRARY_RAW, users_filename_json)
-save_json(users_ordered, users_filepath_json)
+            # Generate whether the user is an active user or not
+            # so that more users will be active by 3:1 proportion
+            'is_active': bool(np.random.choice([True, True, True, False]))
+        }
 
-# Export records to csv
-users_df = pd.DataFrame(users_ordered)
+        # Add user records to final list of users
+        users.append(user)
 
-users_filename_csv = 'users.csv'
-users_filepath_csv = os.path.join(LIBRARY_TABLES, users_filename_csv)
-users_df.to_csv(users_filepath_csv, index=False)
-# ---------------------------------------------------------------------------------------
+    # Order users by timestamp
+    users_ordered = sorted(users, key=lambda u: u['timestamp'])
+
+    # Export raw records to json
+    users_filename_json = 'users.json'
+    users_filepath_json = os.path.join(LIBRARY_RAW, users_filename_json)
+    save_json(users_ordered, users_filepath_json)
+
+    # Export records to csv
+    users_df = pd.DataFrame(users_ordered)
+
+    users_filename_csv = 'users.csv'
+    users_filepath_csv = os.path.join(LIBRARY_TABLES, users_filename_csv)
+    users_df.to_csv(users_filepath_csv, index=False)
+    # ---------------------------------------------------------------------------------------
+
+    logger.info('STAGE_COMPLETE')
