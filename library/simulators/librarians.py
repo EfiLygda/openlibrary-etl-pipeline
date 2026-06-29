a4c00f2a9cb3199772e6f03c42def1a446ffb380
EfiLygda <efi1031995@gmail.com>
2026-06-29 14:49:13 +0300

    Added library/simulators/librarians.py for simulating librarians

---
+++ library/simulators/librarians.py
@@ -0,0 +1,63 @@
+"""
+Librarians simulation module
+"""
+
+import os
+import pandas as pd
+from faker import Faker
+
+from config.paths import LIBRARY_RAW, LIBRARY_TABLES
+from utilities.io.json_io import save_json
+
+from library.simulators.sim_config import SEED, MAX_LIBRARIANS
+from utils.emails import generate_email
+
+# ---------------------------------------------------------------------------------------
+Faker.seed(SEED)
+fake = Faker()
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# --- Librarians ---
+
+# Setting up list of librarians
+librarians = []
+
+for _ in range(MAX_LIBRARIANS):
+
+    # Simulate first and last name
+    first_name, last_name = fake.first_name(), fake.last_name()
+
+    # Simulate librarian record
+    librarian = {
+
+        # Use generated first and last name
+        'first_name': first_name,
+        'last_name': last_name,
+
+        # Use generated first and last name for email generation
+        'email': generate_email(first_name, last_name),
+
+        # Generate registration timestamp from before 3 years to now
+        # using ISO-8601 format (i.e. "2026-06-29T14:32:10")
+        'timestamp': fake.date_time_between(start_date='-3y', end_date='now').isoformat(),
+    }
+
+    # Add librarian records to final list of librarians
+    librarians.append(librarian)
+
+# Order librarians by timestamp
+librarians_ordered = sorted(librarians, key=lambda u: u['timestamp'])
+
+# Export raw records to json
+librarians_filename_json = 'librarians.json'
+librarians_filepath_json = os.path.join(LIBRARY_RAW, librarians_filename_json)
+save_json(librarians_ordered, librarians_filepath_json)
+
+# Export records to csv
+librarians_df = pd.DataFrame(librarians_ordered)
+
+librarians_filename_csv = 'librarians.csv'
+librarians_filepath_csv = os.path.join(LIBRARY_TABLES, librarians_filename_csv)
+librarians_df.to_csv(librarians_filepath_csv, index=False)
+# ---------------------------------------------------------------------------------------
