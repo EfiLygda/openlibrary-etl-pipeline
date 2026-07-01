c08d0294d97eecc5bd246d32df20ad551d7964af
EfiLygda <efi1031995@gmail.com>
2026-07-01 10:00:49 +0300

    Added library/producers/config.py for configuring producer

---
+++ library/producer/config.py
@@ -0,0 +1,71 @@
+"""
+Configuration module for the simulations
+"""
+
+from datetime import datetime
+from utilities.database import db_connection, DB_NAME
+
+# ---------------------------------------------------------------------------------------
+# --- Chosen seed for reproducible results ---
+SEED = 0
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# --- Set up Connection to Database ---
+
+# Establish connection
+connection = db_connection(database=DB_NAME)
+
+# Set up cursor
+cursor = connection.cursor()
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# --- Extract edition keys from the database ---
+
+# Execute SQL query
+cursor.execute("SELECT edition_key FROM editions;")
+
+# Fetch and unpack the edition keys
+EDITION_KEYS = [records[0] for records in cursor.fetchall()]
+
+# Calculate total edition keys
+# NUM_EDITIONS = len(EDITION_KEYS)
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# --- Chosen starting and ending date of simulations ---
+
+START_DATE = datetime(2020, 1,1)
+END_DATE = datetime.now()
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# --- Event weights definitions ---
+
+INITIAL_EVENT_WEIGHTS = {
+    'USER_REGISTERED': 0.35,
+    'COPY_PURCHASED': 0.30,
+    'BORROW': 0.15,
+    'RETURN': 0.10,
+    'RESERVE': 0.08,
+    'LIBRARIAN_HIRED': 0.02
+}
+
+FINAL_EVENT_WEIGHTS = {
+    'USER_REGISTERED': 0.03,
+    'COPY_PURCHASED': 0.08,
+    'BORROW': 0.52,
+    'RETURN': 0.30,
+    'RESERVE': 0.06,
+    'LIBRARIAN_HIRED': 0.01
+}
+# ---------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------
+# Close cursor
+cursor.close()
+
+# Close database connection
+connection.close()
+# ---------------------------------------------------------------------------------------
