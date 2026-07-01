d00f5704441e8bdb2c33041227bba3e998d585cf
EfiLygda <efi1031995@gmail.com>
2026-07-01 10:03:18 +0300

    Added library/producer/utils/clock.py for a clock used for producing event timestamps that simulate time passing in different speeds

---
+++ library/producer/utils/clock.py
@@ -0,0 +1,29 @@
+"""
+Clock used for producing event timestamps that simulate time passing in different speeds
+"""
+
+import time
+from datetime import datetime, timedelta
+
+SIM_SPEED = 1 # 365 * 24 * 60 * 60  # 1 year per second
+
+class SimClock:
+    """
+    Class the simulates a clock that spans a year in a second
+    by passing the starting date
+
+    :param start: datetime.datetime
+    """
+
+    def __init__(self, start: datetime):
+        self.start_real = time.time()
+        self.start_sim = start
+        self.speed = SIM_SPEED
+
+    def now(self) -> datetime:
+        """
+        Function for fetching the simulated time's current timestamp
+        """
+        elapsed_real = time.time() - self.start_real
+        elapsed_sim = elapsed_real * self.speed
+        return self.start_sim + timedelta(seconds=elapsed_sim)
