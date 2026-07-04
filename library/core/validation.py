046b589766066a31e750e89b066cc67d3201de42
EfiLygda <efi1031995@gmail.com>
2026-07-04 17:58:54 +0300

    Added is_over_max_allowed and borrowed_copy_does_not_exist validators for event rejection and reject_event for global event rejection in library/core/validation.py

--- library/core/validation.py
+++ library/core/validation.py
@@ -5,7 +5,7 @@ Event validation module
 from library.core.registry import EVENTS
 from library.service.redis.service import RedisClient
 
-def reject_event(
+def is_over_max_allowed(
         redis_client: RedisClient,
         event: dict
 ) -> bool:
@@ -16,7 +16,7 @@ def reject_event(
         and configuration values
     :param event: dict, event dictionary from Kafka
 
-    :return: bool, true if the event should be rejected, False otherwise.
+    :return: bool, true if the event should be rejected, False otherwise
     """
 
     # Save current event's type
@@ -37,5 +37,50 @@ def reject_event(
     # allowable value then the generated event is rejected
     if counter_value >= max_allowable_value:
         return True
+
+    return False
+
+def borrowed_copy_does_not_exist(event: dict) -> bool:
+    """
+    Determine whether a copy was selected for a borrow event, used for rejecting the event
+
+    :param event: dict, event dictionary from Kafka
+
+    :return: bool, true if the event should be rejected, False otherwise
+    """
+
+    if event['data']['copy_id'] is None:
+        return True
+
+    return  False
+
+def reject_event(
+        redis_client: RedisClient,
+        event: dict
+) -> bool:
+    """
+    Determine whether an event should be rejected or not
+
+    :param redis_client: RedisClient, the redis client used to fetch counters
+        and configuration values
+    :param event: dict, event dictionary from Kafka
+
+    :return: bool, true if the event should be rejected, False otherwise.
+    """
+
+    # Save current event's type
+    event_type = event['event_type']
+
+    # Use maximum allowable event counts for 'LIBRARIAN_HIRED', 'USER_REGISTERED', 'COPY_PURCHASED'
+    # and reject
+    if event_type in ['LIBRARIAN_HIRED', 'USER_REGISTERED', 'COPY_PURCHASED']:
+        return is_over_max_allowed(
+            redis_client=redis_client,
+            event=event
+        )
+
+    # Check if no copy was available to borrow and reject
+    if event_type in ['BORROW']:
+        return borrowed_copy_does_not_exist(event=event)
 
     return False
