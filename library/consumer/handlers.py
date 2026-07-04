04d40af2000d16dd14d2ec2e6d20ce920794841f
EfiLygda <efi1031995@gmail.com>
2026-07-04 03:13:06 +0300

    Reintroduced HANDLERS dispatcher to handlers.py and added handle_event  for handling all events regardless of type

--- library/consumer/handlers.py
+++ library/consumer/handlers.py
@@ -99,3 +99,33 @@ def handle_copy_purchased(
             'registered_at': event['timestamp'],
         }
     )
+
+HANDLERS = {
+    'LIBRARIAN_HIRED': handle_librarian_hired,
+    'USER_REGISTERED': handle_user_registered,
+    'COPY_PURCHASED': handle_copy_purchased,
+
+    # 'BORROW': handle_borrow,
+    # 'RETURN': handle_return,
+    # 'RESERVE': handle_reserve,
+}
+
+def handle_event(
+        connection: psycopg2.extensions.connection,
+        event: dict
+) -> tuple:
+    """
+    Function for handling all events regardless of type, by inserting new records
+
+    :param connection: psycopg2.extensions.connection, the connection used for inserting the new record
+    :param event: dict, the event/dictionary used
+
+    :return: tuple, the tuple containing:
+        * `data` - list of matching records returned by the query
+        * `data_column_names` - column names corresponding to the records
+    """
+
+    return HANDLERS[event['event_type']](
+        connection=connection,
+        event=event
+    )
