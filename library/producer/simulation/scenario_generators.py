ce0b9e6a1a34b1772d061572d616665393cb3c76
EfiLygda <efi1031995@gmail.com>
2026-07-04 17:57:38 +0300

    Added random selection of user, copy and librarian ids from redis for generating 'BORROW' events in scenario_generators.py

--- library/producer/simulation/scenario_generators.py
+++ library/producer/simulation/scenario_generators.py
@@ -64,29 +64,29 @@ def copy_purchased() -> dict:
     """
 
     # Choose a random edition for purchasing a copy
-    edition_key = redis_client.get_random_from_set('editions')
+    edition_key = redis_client.get_random_from_set('editions:keys')
 
     return {
         # Use edition key
         'edition_key': str(edition_key),
     }
 
-def borrow(
-        user_id: int,
-        copy_id: str,
-) -> dict:
+def borrow() -> dict:
     """
     Simulates a copy's borrowing from a user
-
-    :param user_id: str, the user's id that borrowed the copy
-    :param copy_id: str, the copy's id that was borrowed
 
     :return: dict, dictionary with the user_id, the copy_id, and the due date for returning the copy
     """
 
+    # Choose a random user and copy
+    user_id = redis_client.get_random_from_set('users:ids')
+    copy_id = redis_client.get_random_from_set('copies:available:ids')
+    librarian_id = redis_client.get_random_from_set('librarians:ids')
+
     return {
         'user_id': user_id,
         'copy_id': copy_id,
+        'librarian_id': librarian_id,
         'due_date': (CLOCK.now() + timedelta(days=7)).isoformat()
     }
 
