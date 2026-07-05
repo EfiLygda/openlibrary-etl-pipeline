d16b9abdbb6af4213d7f58a456a9d124ad22c2b1
EfiLygda <efi1031995@gmail.com>
2026-07-05 21:08:37 +0300

    Added handle_return_borrowed_copy in handlers.py for RETURN events and return_update_loans_update_copies.sql used in this handler.

---
+++ library/consumer/sql/return_update_loans_update_copies.sql
@@ -0,0 +1,18 @@
+UPDATE
+    loans
+SET
+    status = 'RETURNED',
+    return_date = %(return_date)s
+WHERE
+    loan_id = %(loan_id)s
+RETURNING
+    loan_id;
+
+UPDATE
+    copies
+SET
+    status = 'AVAILABLE'
+WHERE
+    copy_id = %(copy_id)s
+RETURNING
+    copy_id;
