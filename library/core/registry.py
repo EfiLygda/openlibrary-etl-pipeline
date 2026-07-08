2b422fe1cfb414654e3f4616e3ab9da93a613e0d
EfiLygda <efi1031995@gmail.com>
2026-07-08 19:15:12 +0300

    Added new produces_event attribute in EventSpec as a flag for events that can emit chain events

--- library/core/registry.py
+++ library/core/registry.py
@@ -22,22 +22,25 @@ class EventSpec:
             self,
             counter_name: Callable[[dict], str],
             max_allowable_name: Callable[[dict], str] | None,
+            produces_event: bool,
     ):
         self.counter_name = counter_name
         self.max_allowable_name = max_allowable_name
-
+        self.produces_event = produces_event
 
 EVENTS = {
 
     # --- People ---
     'LIBRARIAN_HIRED': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.LIBRARIANS,
-        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.LIBRARIANS
+        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.LIBRARIANS,
+        produces_event=False,
     ),
 
     'USER_REGISTERED': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.USERS,
-        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.USERS
+        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.USERS,
+        produces_event=False,
     ),
 
     # --- Inventory ---
@@ -45,28 +48,33 @@ EVENTS = {
         counter_name=lambda event:
             RedisKeys.Counters.edition_copies(event['data']['edition_key']),
         max_allowable_name=lambda event:
-            RedisKeys.MaxAllowableValues.edition_copies(event['data']['edition_key'])
-    ),
+            RedisKeys.MaxAllowableValues.edition_copies(event['data']['edition_key']),
+        produces_event=False,
+        ),
 
     # --- Circulation ---
     'BORROW': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.LOANS,
-        max_allowable_name=None
+        max_allowable_name=None,
+        produces_event=False,
     ),
 
     'RETURN': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.RETURNS,
-        max_allowable_name=None
+        max_allowable_name=None,
+        produces_event=True, # In case of reserved copy emits BORROW from the user that reserved it
     ),
 
     'RENEWAL': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.RENEWALS,
-        max_allowable_name=None
+        max_allowable_name=None,
+        produces_event=False,
     ),
 
     # --- Demand ---
     'RESERVATION': EventSpec(
         counter_name=lambda event: RedisKeys.Counters.RESERVATIONS,
-        max_allowable_name=None
+        max_allowable_name=None,
+        produces_event=False,
     ),
 }
