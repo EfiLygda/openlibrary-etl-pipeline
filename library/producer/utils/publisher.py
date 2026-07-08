972e22458ce8ed56e0678b6bf28870565db9f3b7
EfiLygda <efi1031995@gmail.com>
2026-07-08 18:41:53 +0300

    Added library/producer/utils/publisher.py for Kafka event publishing utilities

---
+++ library/producer/utils/publisher.py
@@ -0,0 +1,30 @@
+"""
+Kafka event publishing utilities
+"""
+
+import kafka
+
+def emit_event(
+        producer: kafka.producer.kafka.KafkaProducer,
+        topic: str,
+        event: dict
+) -> None:
+    """
+    Publish an event to the configured Kafka topic
+
+    The event is sent asynchronously to the configured topic and then
+    immediately flushed to ensure it is available to consumers
+
+    :param producer: kafka.producer.kafka.KafkaProducer, the Kafka producer
+        instance used to publish the event
+    :param topic: str, the Kafka topic to publish the event to
+    :param event: dict, the event dictionary to publish
+
+    :return: None.
+    """
+    # Publish the allowed event to chosen topic
+    producer.send(topic, value=event)
+
+    # All buffered events are immediately available
+    # TODO: add batches
+    producer.flush()
