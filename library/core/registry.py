db38faea11264f70ba7e0c99155a2cf4e27c0d26
EfiLygda <efi1031995@gmail.com>
2026-07-13 16:48:14 +0300

    Redesigned registry.py to work with new redis key conventions

--- library/core/registry.py
+++ library/core/registry.py
@@ -1,7 +1,6 @@
 """
 Central registry of event metadata shared between the producer and consumer
 """
-
 from typing import Callable
 
 from library.service.redis.keys import RedisKeys
@@ -24,11 +23,18 @@ class EventSpec:
 
     def __init__(
             self,
+
             category: EventCategory,
+
             generator: Callable[..., dict],
             handler: Callable[..., None],
-            counter_name: Callable[[dict], str] | None,
-            max_allowable_name: Callable[[dict], str] | None,
+
+            counter_name: str | None,
+            counter_hash_key: Callable[[dict], str] | None,
+
+            max_allowable_name: str | None,
+            max_allowable_hash_key: Callable[[dict], str] | None,
+
             produces_event: bool,
     ):
 
@@ -41,10 +47,12 @@ class EventSpec:
 
         # The event's redis counter name
         self.counter_name = counter_name
+        self.counter_hash_key = counter_hash_key
 
         # The event's redis key for the maximum allowable times
         # it should be generated
         self.max_allowable_name = max_allowable_name
+        self.max_allowable_hash_key = max_allowable_hash_key
 
         # Whether the event produces another event, or not
         self.produces_event = produces_event
@@ -58,8 +66,11 @@ EVENTS = {
         generator=generators.people.librarian_hired,
         handler=handlers.people.handle_librarian_hired,
 
-        counter_name=lambda event: RedisKeys.Counters.LIBRARIANS,
-        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.LIBRARIANS,
+        counter_name=RedisKeys.Counters.LIBRARIANS,
+        counter_hash_key=None,
+
+        max_allowable_name=RedisKeys.Strings.MAX_LIBRARIANS,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
@@ -70,8 +81,11 @@ EVENTS = {
         generator=generators.people.user_registration,
         handler=handlers.people.handle_user_registered,
 
-        counter_name=lambda event: RedisKeys.Counters.USERS,
-        max_allowable_name=lambda event: RedisKeys.MaxAllowableValues.USERS,
+        counter_name=RedisKeys.Counters.USERS,
+        counter_hash_key=None,
+
+        max_allowable_name=RedisKeys.Strings.MAX_USERS,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
@@ -83,10 +97,11 @@ EVENTS = {
         generator=generators.inventory.copy_purchased,
         handler=handlers.inventory.handle_copy_purchased,
 
-        counter_name=lambda event:
-            RedisKeys.Counters.edition_copies(event['data']['edition_key']),
-        max_allowable_name=lambda event:
-            RedisKeys.MaxAllowableValues.edition_copies(event['data']['edition_key']),
+        counter_name=RedisKeys.Hashes.COUNTER_EDITION_COPIES,
+        counter_hash_key=lambda event: event['data']['edition_key'],
+
+        max_allowable_name=RedisKeys.Hashes.MAX_EDITION_COPIES,
+        max_allowable_hash_key=lambda event: event['data']['edition_key'],
 
         produces_event=False,
         ),
@@ -98,8 +113,11 @@ EVENTS = {
         generator=generators.circulation.borrow_available_copy,
         handler=handlers.circulation.handle_copy_borrowed,
 
-        counter_name=lambda event: RedisKeys.Counters.LOANS,
+        counter_name=RedisKeys.Counters.LOANS,
+        counter_hash_key=None,
+
         max_allowable_name=None,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
@@ -110,8 +128,11 @@ EVENTS = {
         generator=generators.circulation.return_copy,
         handler=handlers.circulation.handle_return_borrowed_copy,
 
-        counter_name=lambda event: RedisKeys.Counters.RETURNS,
+        counter_name=RedisKeys.Counters.RETURNS,
+        counter_hash_key=None,
+
         max_allowable_name=None,
+        max_allowable_hash_key=None,
 
         produces_event=True, # In case of reserved copy emits BORROW from the user that reserved it
     ),
@@ -122,8 +143,11 @@ EVENTS = {
         generator=generators.circulation.renew_loan,
         handler=handlers.circulation.handle_renewal_of_borrowed_copy,
 
-        counter_name=lambda event: RedisKeys.Counters.RENEWALS,
+        counter_name=RedisKeys.Counters.RENEWALS,
+        counter_hash_key=None,
+
         max_allowable_name=None,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
@@ -135,8 +159,11 @@ EVENTS = {
         generator=generators.demand.reserve_unavailable_copy,
         handler=handlers.demand.handle_reservation_of_unavailable_copy,
 
-        counter_name=lambda event: RedisKeys.Counters.RESERVATIONS,
+        counter_name=RedisKeys.Counters.RESERVATIONS,
+        counter_hash_key=None,
+
         max_allowable_name=None,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
@@ -147,8 +174,11 @@ EVENTS = {
         generator=generators.demand.cancel_reservation,
         handler=handlers.demand.handle_cancellation_of_active_reservation,
 
-        counter_name=lambda event: RedisKeys.Counters.CANCELLED_RESERVATIONS,
+        counter_name=RedisKeys.Counters.CANCELLED_RESERVATIONS,
+        counter_hash_key=None,
+
         max_allowable_name=None,
+        max_allowable_hash_key=None,
 
         produces_event=False,
     ),
