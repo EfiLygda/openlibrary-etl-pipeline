ff17aa646fe019bc019444f48bf7f68153cf1082
EfiLygda <efi1031995@gmail.com>
2026-07-13 16:47:48 +0300

    Redesigned redis keys.py as to use hashes for edition's counters and maximum allowable edition copies

--- library/service/redis/keys.py
+++ library/service/redis/keys.py
@@ -10,20 +10,35 @@ Redis naming conventions:
     - copies:unavailable:ids (SET) - runtime copy ids that are not available to borrow
     - loans:active:ids (SET) - runtime loan ids that are active
     - loans:returned:ids (SET) - runtime loan ids with returned copies
+    - reservations:active:ids (SET) - runtime reservation ids that are active
+    - reservations:fulfilled:ids (SET) - runtime reservation ids that are fulfilled
+    - reservations:canceled:ids (SET) - runtime reservation ids that are canceled
 
 * Maximum Allowable Values
     - max:users (INT) - max users to register
     - max:librarians (INT) - max librarians to hire
-    - max:edition:{edition_key}:copies (INT) - max copies for current edition_key
+    - hash:max:edition:copies (HASH)
+        - field: edition_key
+        - value: max copies allowed for current edition_key
 
 * Runtime Counters
     - counter:users (INCR) - counting current registered users
     - counter:librarians (INCR) - counting current hired librarians
-    - counter:edition:{edition_key}:copies (INCR) - counting current edition's copies purchased
+    - hash:counter:edition:copies (HASH)
+        - field: edition_key
+        - value: counting current edition's copies purchased
     - counter:loans (INCR) - counting all borrowings of all copies
+    - counter:returns (INCR) - counting all returned loans
+    - counter:renewals (INCR) - counting all loan renewals
+    - counter:reservations (INCR) - counting all created reservations
+    - counter:reservations:canceled (INCR) - counting all canceled reservations
 """
 
 class RedisKeys:
+
+    class Strings:
+        MAX_USERS = 'max:users'
+        MAX_LIBRARIANS = 'max:librarians'
 
     class Sets:
         EDITION_KEYS = 'editions:keys'
@@ -42,14 +57,6 @@ class RedisKeys:
         CANCELLED_RESERVATIONS_IDS = 'reservations:canceled:ids'
         # EXPIRED_RESERVATIONS_IDS = 'reservations:active:ids'
 
-    class MaxAllowableValues:
-        USERS = 'max:users'
-        LIBRARIANS = 'max:librarians'
-
-        @staticmethod
-        def edition_copies(edition_key: str) -> str:
-            return f'max:edition:{edition_key}:copies'
-
     class Counters:
         USERS = 'counter:users'
         LIBRARIANS = 'counter:librarians'
@@ -59,11 +66,10 @@ class RedisKeys:
         RESERVATIONS = 'counter:reservations'
         CANCELLED_RESERVATIONS = 'counter:reservations:canceled'
 
-        @staticmethod
-        def edition_copies(edition_key: str) -> str:
-            return f'counter:edition:{edition_key}:copies'
-
     class Hashes:
+
+        MAX_EDITION_COPIES = 'hash:max:edition:copies'
+        COUNTER_EDITION_COPIES = 'hash:counter:edition:copies'
 
         @staticmethod
         def loan(loan_id: str) -> str:
