c8f536b113ba81e103d54cfbbdc804b75a809330
EfiLygda <efi1031995@gmail.com>
2026-07-13 11:53:59 +0300

    Removed generator mapping from event_generator.py, by using the EVENTS spec from registry.py

--- library/producer/simulation/event_generator.py
+++ library/producer/simulation/event_generator.py
@@ -2,23 +2,12 @@
 Module responsible for generating simulation events
 """
 
+from library.core.registry import EVENTS
 from library.service.redis.client import RedisClient
 from library.producer.producer_config import CLOCK
 from library.producer.simulation.event_selection import get_event_by_timeline
 from library.producer.simulation.event_factory import create_event
 from library.producer.simulation import generators
-
-# Mapping event names to their respective generation functions
-EVENT_GENERATION_MAPPINGS = {
-    'USER_REGISTERED': generators.people.user_registration,
-    'LIBRARIAN_HIRED': generators.people.librarian_hired,
-    'COPY_PURCHASED': generators.inventory.copy_purchased,
-    'BORROW': generators.circulation.borrow_available_copy,
-    'RETURN': generators.circulation.return_copy,
-    'RENEWAL': generators.circulation.renew_loan,
-    'RESERVATION': generators.demand.reserve_unavailable_copy,
-    'CANCELLED_RESERVATION': generators.demand.cancel_reservation,
-}
 
 def generate_event(redis_client: RedisClient) -> dict:
     """
@@ -43,8 +32,8 @@ def generate_event(redis_client: RedisClient) -> dict:
         timestamp=timestamp
     )
 
-    # Generate the event's data via its event type
-    data = EVENT_GENERATION_MAPPINGS[event_type](redis_client=redis_client)
+    # Generate the event's data via its event spec
+    data = EVENTS[event_type].generator(redis_client=redis_client)
 
     return create_event(
         event_type=str(event_type),
