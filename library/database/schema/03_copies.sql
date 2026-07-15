1ad282042f1e6ef091f12b392d08db26d3a0b6e9
EfiLygda <efi1031995@gmail.com>
2026-07-15 11:12:35 +0300

    Added missing foreign key in copies.sql creation file

--- library/database/schema/03_copies.sql
+++ library/database/schema/03_copies.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS copies (
     copy_id         TEXT PRIMARY KEY,
     edition_key     TEXT,
     status          TEXT,
-    registered_at   TIMESTAMP
+    registered_at   TIMESTAMP,
+
+    CONSTRAINT fk_copies_edition_key
+        FOREIGN KEY (edition_key)
+        REFERENCES editions(edition_key)
 );
