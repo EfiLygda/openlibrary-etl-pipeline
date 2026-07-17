5808765f2bae740c80b0bf73ff3fd5947ef807aa
EfiLygda <efi1031995@gmail.com>
2026-07-17 15:29:29 +0300

    Added overdue_days in fines tables

--- library/database/schema/06_fines.sql
+++ library/database/schema/06_fines.sql
@@ -5,15 +5,16 @@ CREATE TYPE
         );
 
 CREATE TABLE IF NOT EXISTS fines (
-    fine_id   TEXT PRIMARY KEY,
-    loan_id   TEXT NOT NULL,
+    fine_id         TEXT PRIMARY KEY,
+    loan_id         TEXT NOT NULL,
 
-    amount    NUMERIC(8,2),
+    overdue_days    INTEGER,
+    amount          NUMERIC(8,2),
 
-    issued_at TIMESTAMP,
-    paid_at   TIMESTAMP,
+    issued_at       TIMESTAMP,
+    paid_at         TIMESTAMP,
 
-    status    fine_status,
+    status          fine_status,
 
     CONSTRAINT fk_fines_loan_id
         FOREIGN KEY (loan_id)
