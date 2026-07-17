33d465194cb316ebdc7ad66789068e69a030d9a7
EfiLygda <efi1031995@gmail.com>
2026-07-17 14:03:27 +0300

    Added new table fines

---
+++ library/database/schema/06_fines.sql
@@ -0,0 +1,21 @@
+CREATE TYPE
+    fine_status AS ENUM (
+            'PAID',
+            'UNPAID'
+        );
+
+CREATE TABLE IF NOT EXISTS fines (
+    fine_id   TEXT PRIMARY KEY,
+    loan_id   TEXT NOT NULL,
+
+    amount    NUMERIC(8,2),
+
+    issued_at TIMESTAMP,
+    paid_at   TIMESTAMP,
+
+    status    fine_status,
+
+    CONSTRAINT fk_fines_loan_id
+        FOREIGN KEY (loan_id)
+        REFERENCES loans(loan_id)
+);
