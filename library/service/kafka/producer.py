5dce04664ec55c2af19bd5050dcacc316a9d9379
EfiLygda <efi1031995@gmail.com>
2026-07-21 14:05:15 +0300

    Added new JsonSerializer class in library/service/kafka/producer.py in order to fix deprecating warning about using functions as producer serializers

--- library/service/kafka/producer.py
+++ library/service/kafka/producer.py
@@ -7,7 +7,28 @@ instances used by the application to publish events.
 
 import json
 from kafka import KafkaProducer
+from kafka.serializer import Serializer
 from library.service.kafka.config import BOOTSTRAP
+
+class JsonSerializer(Serializer):
+
+    def serialize(
+            self,
+            topic: str,
+            data: dict,
+            header: None = None
+    ):
+        """
+        Serialize a message value into UTF-8 encoded JSON bytes.
+
+        :param topic: str, Kafka topic the message is being published to.
+        :param data: dict, dictionary containing the event payload.
+        :param header: None, unimplemented header for event's metadata.
+
+        :return: Serialized message as UTF-8 encoded bytes.
+        """
+
+        return json.dumps(data).encode("utf-8")
 
 def create_producer() -> KafkaProducer:
     """
@@ -20,5 +41,5 @@ def create_producer() -> KafkaProducer:
     """
     return KafkaProducer(
         bootstrap_servers=BOOTSTRAP,
-        value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        value_serializer=JsonSerializer()
     )
