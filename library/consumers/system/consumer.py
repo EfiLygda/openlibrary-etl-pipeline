a7c155df5ea5ff17d9fac2b879f6cd7fe65a2a39
EfiLygda <efi1031995@gmail.com>
2026-07-21 13:50:07 +0300

    Added optional flag display-events in system consumer.py and also logging

--- library/consumers/system/consumer.py
+++ library/consumers/system/consumer.py
@@ -22,10 +22,17 @@ Workflow:
 Run:
     python -m library.consumers.system.consumer.py
 """
+import os
+import time
+import logging
+import argparse
+from dotenv import load_dotenv
+
+from config.paths import LOG_DIR
 
 from utilities.database import db_connection, DB_NAME
+from utilities.logger import config_logger, set_logger
 
-from library.core.validation import reject_event
 from library.utils.event_display import print_event
 
 from library.service.redis.client import RedisClient
@@ -37,6 +44,55 @@ from library.service.kafka.producer import create_producer
 
 from library.consumers.system.handlers.dependencies import HandlerDependencies
 from library.consumers.system.handlers_dispatcher import handle_event
+
+# ----------------------------------------------------------------------------------
+# --- Load Environment Variables ---
+# Load variables from the .env file to the environment
+load_dotenv()
+
+# Setting up the genre
+LOG_LEVEL = os.getenv("LOG_LEVEL")
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# --- Setting up logging ---
+# Log filepath
+log_filepath = os.path.join(LOG_DIR, 'system_consumer.log')
+
+# Configure the logger (uses console and file for log records)
+config_logger(filepath=log_filepath, level=LOG_LEVEL)
+
+# Setting up the logger
+logger = set_logger('SYSTEM_CONSUMER')
+
+# Silencing 'redis' logging to level 'WARNING'
+logging.getLogger("redis").setLevel(logging.WARNING)
+logging.getLogger("redis.connection").setLevel(logging.WARNING)
+logging.getLogger("redis.client").setLevel(logging.WARNING)
+
+# Silencing 'kafka' logging to level 'WARNING'
+logging.getLogger("kafka").setLevel(logging.WARNING)
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# --- Setting up command line argument parser ---
+
+# Setting up argument parser via the command line
+parser = argparse.ArgumentParser()
+
+# Add `display-events` flag for displaying full event envelope in console
+parser.add_argument(
+    "--display-events",
+    action="store_true",
+    help="Display Kafka events during simulation"
+)
+
+# Parse command line arguments
+args = parser.parse_args()
+# ----------------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------------
+# --- Setting up consumer's handlers' dependencies ---
 
 # Establish database connection
 connection = db_connection(database=DB_NAME)
@@ -53,13 +109,16 @@ handler_dependencies = HandlerDependencies(
     redis_operations=RedisOperations(redis_client),
     chain_event_producer=chain_event_producer
 )
+# ----------------------------------------------------------------------------------
 
-# Setting up system of events
+# ----------------------------------------------------------------------------------
+# --- Setting up current consumer ---
 consumer = create_consumer(
     topic=TOPIC,
     consumer_name='library-system-consumer',
     group_id=CONSUMER_GROUP_ID
 )
+# ----------------------------------------------------------------------------------
 
 # For each message/event in the topic
 # the system fetches the event message
@@ -69,16 +128,62 @@ for msg in consumer:
     # Save the event's message
     event = msg.value
 
-    # Display event
-    print_event(
-        source='SYSTEM_CONSUMER',
-        event=event,
+    # Save current time for consumer performance benchmarking
+    start_time = time.perf_counter()
+
+    # Log received event in real time (NOT simulated)
+    logger.info(
+        f'EVENT_RECEIVED '
+        f'event_type={event['event_type']} '
+        f'event_id={event['event_id']} '
+        f'topic={msg.topic} '
+        f'partition={msg.partition} '
+        f'offset={msg.offset} '
+        f'group_id={CONSUMER_GROUP_ID} '
     )
 
-    # TODO: add bulk loading of db at end of day
-    # If current event type can be handled then use the proper
-    # handler and load data to database
-    handle_event(
-        dependencies=handler_dependencies,
-        event=event,
-    )
+    try:
+
+        # Display event
+        if args.display_events:
+            print_event(
+                source='SYSTEM_CONSUMER',
+                event=event,
+            )
+
+        # TODO: add bulk loading of db at end of day
+        # If current event type can be handled then use the proper
+        # handler and load data to database
+        handle_event(
+            dependencies=handler_dependencies,
+            event=event,
+        )
+
+        # Log processed event in real time (NOT simulated)
+        logger.info(
+            f'EVENT_PROCESSED '
+            f'event_type={event['event_type']} '
+            f'event_id={event['event_id']} '
+            f'topic={msg.topic} '
+            f'partition={msg.partition} '
+            f'offset={msg.offset} '
+            f'group_id={CONSUMER_GROUP_ID} '
+            f'duration_ms={(time.perf_counter() - start_time) * 1000:.2f}'
+        )
+
+    except Exception as e:
+
+        # Display event
+        if args.display_events:
+            print_event(
+                source='SYSTEM_CONSUMER_PROCESSING_ERROR',
+                event=event,
+            )
+
+        # Log processing error for event in real time (NOT simulated)
+        logger.error(
+            f'EVENT_PROCESSING_FAILED '
+            f'event_type={event['event_type']} '
+            f'event_id={event['event_id']} '
+            f'error_type={e.__class__.__name__}'
+        )
